834664031a68e8bdd1095fdad0d40afb129fb81e
hide <sam.hide.ettahiri@gmail.com>
2019-03-18 15:26:19 +0100

    first try to get games

--- app/providers/smash_gg/get_tournaments_by_country.rb
+++ app/providers/smash_gg/get_tournaments_by_country.rb
@@ -1,4 +1,10 @@
 class SmashGg::GetTournamentsByCountry
+  # Melee ID: 1
+  # Tekken ID: 17
+  # Ultimate ID : 1386
+  # MarioKart8 Deluxe : 2165
+  #
+
   TournamentsQuery = GraphClient.parse <<-'GRAPHQL'
     query($cCode: String!, $perPage: Int!) {
       tournaments(query: {  perPage: $perPage,
@@ -16,6 +22,12 @@ class SmashGg::GetTournamentsByCountry
                 mapsPlaceId
                 venueAddress
                 venueName
+                events {
+                  videogame {
+                    id
+                    name
+            }  
+          }
         }
       }
     }
@@ -24,6 +36,10 @@ class SmashGg::GetTournamentsByCountry
   def self.call(country: "FR", page_size: 25)
      results = GraphClient.query(TournamentsQuery, variables: {cCode: country, perPage: page_size}).original_hash.dig('data' ,'tournaments', 'nodes')
      format(results.dup)
+  end
+
+  def self.videogames(hash)
+    hash['events'].map{|e| e.dig('name')}.uniq
   end
 
   def self.format(results)
@@ -36,6 +52,7 @@ class SmashGg::GetTournamentsByCountry
       new_hash[:venue_name] = new_hash.delete('venueName')
       new_hash[:address] = new_hash.delete('venueAddress')
       new_hash[:google_place_id] = new_hash.delete('mapsPlaceId')
+      new_hash[:games] = videogames(hash)
       new_hash.with_indifferent_access
     end 
   end
